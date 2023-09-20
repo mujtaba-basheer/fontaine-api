c16544d21e22e1febe4ad20af8bdece18de74228
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2023-09-20 22:21:52 +0530

    ip :-/

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import { notFound, errorHandler } from "./middleware/error.js";
 
 config();
 const app = express();
+app.set("trust proxy", true);
 // app.use(morgan("dev"));
 app.use(cors());
 app.use((req, res, next) => {
